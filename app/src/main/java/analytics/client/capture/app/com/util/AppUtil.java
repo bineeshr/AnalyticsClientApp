71db3cc26f4bdfbb726ee4e0fb43a24bb0f7ddaa
“bineeshr” <bineesh256@gmail.com>
2018-09-01 21:36:20 +0530

    UserSwitch

--- app/src/main/java/analytics/client/capture/app/com/util/AppUtil.java
+++ app/src/main/java/analytics/client/capture/app/com/util/AppUtil.java
@@ -1,24 +1,26 @@
 package analytics.client.capture.app.com.util;
 
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 
-import analytics.client.capture.app.com.workmanger.CaptureAnalyticsWork;
+import analytics.client.capture.app.com.workmanger.CaptureService;
 
 public class AppUtil {
     private static final int JOB_ID_UPDATE = 0x1000;
 
     public static void scheduleJob(Context context) {
-        ComponentName serviceComponent = new ComponentName(context, CaptureAnalyticsWork.class);
-        JobInfo job =
-                new JobInfo.Builder(JOB_ID_UPDATE,
-                        serviceComponent).setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
-                        .setRequiresCharging(true)
-                        .build();
-        JobScheduler mJobService = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
-        mJobService.schedule(job);
+//        ComponentName serviceComponent = new ComponentName(context, CaptureAnalyticsWork.class);
+//        JobInfo job =
+//                new JobInfo.Builder(JOB_ID_UPDATE,
+//                        serviceComponent).setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+//                        .setRequiresCharging(true)
+//                        .build();
+//        JobScheduler mJobService = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+//        mJobService.schedule(job);
+
+
+        Intent i = new Intent(context, CaptureService.class);
+        context.startService(i);
     }
 
 
